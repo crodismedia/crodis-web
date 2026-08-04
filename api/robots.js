756944eb423641af1af8014247cd64496d5fb1b3
crodismedia <crodismedia@outlook.es>
2026-08-04 17:13:01 +0200

    Servir robots txt de forma estable en Vercel

---
+++ api/robots.js
@@ -0,0 +1,13 @@
+export default function handler(_request, response) {
+    response.setHeader("Content-Type", "text/plain; charset=utf-8");
+    response.setHeader("Cache-Control", "public, s-maxage=3600, stale-while-revalidate=86400");
+    response.status(200).send([
+        "User-agent: *",
+        "Allow: /",
+        "",
+        "Sitemap: https://www.tallermap.es/sitemap-index.xml",
+        "Host: www.tallermap.es",
+        "",
+        "# Índice principal actualizado 2026-08-04"
+    ].join("\n"));
+}
