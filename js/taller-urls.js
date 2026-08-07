d5ef107d026d36d6b9512df6b2b32a06f80c748e
crodismedia <crodismedia@outlook.es>
2026-08-07 08:59:51 +0200

    Reviews: cargar módulo público desde las fichas

--- js/taller-urls.js
+++ js/taller-urls.js
@@ -11,6 +11,20 @@
     document.head.appendChild(script);
   }
 
+  function cargarEstilo(href, atributo) {
+    if (document.querySelector(`link[${atributo}]`)) return;
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = href;
+    link.setAttribute(atributo, "true");
+    document.head.appendChild(link);
+  }
+
   cargar("/js/imagenes-automaticas.js", "data-tallermap-imagenes-auto");
   cargar("/js/taller-urls-core.js", "data-tallermap-urls-core");
+
+  if (window.location.pathname === "/pages/taller.html" || window.location.pathname.startsWith("/talleres/")) {
+    cargarEstilo("/css/valoraciones.css", "data-tallermap-valoraciones-css");
+    cargar("/js/valoraciones.js", "data-tallermap-valoraciones");
+  }
 }());
