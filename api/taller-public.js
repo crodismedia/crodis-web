d0600ada7801e2bad12122112ae52d7c9bf50a43
crodismedia <crodismedia@outlook.es>
2026-08-23 01:43:02 +0200

    Diferir CSS secundario de fichas

--- api/taller-public.js
+++ api/taller-public.js
@@ -1,9 +1,21 @@
 import tallerHandler from "./taller-html.js";
 
+function diferirCSSSecundario(html) {
+    return html
+        .replace(
+            /<link rel="stylesheet" href="(\/css\/valoraciones\.css(?:\?[^\"]*)?)">/i,
+            '<link rel="stylesheet" href="$1" media="print" onload="this.media=\'all\'"><noscript><link rel="stylesheet" href="$1"></noscript>'
+        )
+        .replace(
+            /<link rel="stylesheet" href="(\/css\/taller-botones-contexto\.css(?:\?[^\"]*)?)">/i,
+            '<link rel="stylesheet" href="$1" media="print" onload="this.media=\'all\'"><noscript><link rel="stylesheet" href="$1"></noscript>'
+        );
+}
+
 function prepararFichaCanonica(html) {
     if (typeof html !== "string") return html;
 
-    return html
+    const output = html
         .replace(
             /\s*<script[^>]+src="[^"]*taller-legacy-redirect\.js[^"]*"[^>]*><\/script>/gi,
             ""
@@ -12,6 +24,8 @@ function prepararFichaCanonica(html) {
             /\s*<script[^>]+src="https:\/\/(?:unpkg\.com|cdn\.jsdelivr\.net)\/[^"]*supabase[^"]*"[^>]*><\/script>/gi,
             ""
         );
+
+    return diferirCSSSecundario(output);
 }
 
 export default async function handler(request, response) {
