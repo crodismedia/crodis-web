037ac4d7db301b9d85dc760928bc2a086bb01286
crodismedia <crodismedia@outlook.es>
2026-08-23 08:56:27 +0200

    Simplificar wrapper de ficha de desguace

--- api/desguace-public.js
+++ api/desguace-public.js
@@ -1,18 +1,1 @@
-import desguaceHandler from "./desguace-html.js";
-
-function limpiarEnlacesTecnicos(html) {
-    if (typeof html !== "string") return html;
-
-    return html.replace(
-        /<a class="btn" href="(\/solicitar-pieza\.html\?desguace=[^"]+)">Solicitar pieza<\/a>/gi,
-        '<a class="btn" href="$1" rel="nofollow">Solicitar pieza</a>'
-    );
-}
-
-export default async function handler(request, response) {
-    const originalSend = response.send.bind(response);
-
-    response.send = (body) => originalSend(limpiarEnlacesTecnicos(body));
-
-    return desguaceHandler(request, response);
-}
+export { default } from "./desguace-html.js";
