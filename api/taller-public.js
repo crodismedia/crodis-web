c531a9c915400c0290b76daff3226bd29be5543f
crodismedia <crodismedia@outlook.es>
2026-09-03 03:06:57 +0200

    Aplicar footer compacto a todas las fichas de taller

--- api/taller-public.js
+++ api/taller-public.js
@@ -1,10 +1,11 @@
 import tallerHandler from "./taller-html.js";
 
-const TALLER_SHELL_VERSION = "20260823-2";
+const TALLER_SHELL_VERSION = "20260903-1";
 const VALORACIONES_VERSION = "20260823-2";
 const RECLAMACION_VERSION = "20260823-2";
 const VALORACIONES_CSS_VERSION = "20260823-1";
 const RENDER_DIFERIDO = '<style id="tm-render-diferido">.ficha-contexto,.ficha-relacionados,.valoraciones-seccion{content-visibility:auto;contain-intrinsic-size:auto 500px}</style>';
+const FOOTER_COMPACTO = '<style id="tm-footer-compacto">.pie{min-height:0!important;padding:22px 0 18px!important}.pie .copyright{margin-top:0!important;padding-top:0!important;border-top:0!important}</style>';
 
 function usarCSSLigero(html) {
     return html.replace(
@@ -46,7 +47,7 @@ function versionarScripts(html) {
 
 function optimizarRenderDiferido(html) {
     if (html.includes('id="tm-render-diferido"')) return html;
-    return html.replace(/<\/head>/i, `${RENDER_DIFERIDO}\n</head>`);
+    return html.replace(/<\/head>/i, `${RENDER_DIFERIDO}\n${FOOTER_COMPACTO}\n</head>`);
 }
 
 function limpiarContenidoPublico(html) {
